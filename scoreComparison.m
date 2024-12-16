b14e9bd39b92ce1837ee3816197f1a5dcdffa971
brandonhackney <bhackney@uci.edu>
2024-12-16 11:47:26 -0800

    Clean up R2 reporting

--- scoreComparison.m
+++ scoreComparison.m
@@ -4,43 +4,23 @@ function labelStruct = scoreComparison(modelFits, labelStruct, predictorList)
 % Return the best model (and associated R2) per ROI
 numModels = size(modelFits, 3);
 numPreds = length(predictorList);
+numROIs = size(modelFits, 2);
 
-% This compares each model for each ROI, within a given run,
-% then selects the model chosen most often across all runs (per ROI).
-% so that you get a set of "ROI X is best explained by parameter Y"
-[~, a] = max(modelFits, [], 3); % find max value of each model comparison
-results = mode(a, 1); % across runs, which model wins most often per ROI?
-clear a
-
-% An alternative method is to average the results across subjects,
-% then decide which model has the maximum value per ROI.
+% Get the average model fits across subjects,
+% so we can see which model has the best fit in each ROI.
 modelAvgs = mean(modelFits, 1);
 
 % Now you have a 1xn vector of model numbers per ROI
 % Write that, and the associated R2 (or average R2 I guess), to the struct
-assert(length(labelStruct) == length(results), 'Model fits and labels have unequal number of ROIs!');
-roimeans = [];
-roimaxes = [];
-for i = 1:length(results) 
-    labelStruct(i).BestModel = results(i);
-    x = modelFits(:, i, results(i));
-    labelStruct(i).R2 = mean(x, 1); % also export R2 val
-    % Above, we're taking the best model per run per ROI,
-    % then averaging the max R2 across all runs of that model.
-    % (As opposed to subsetting to just the specific runs when that model
-    % was the best).
-    [~, bestModel(i)] = max(modelAvgs(:,i,:));
-    [~, worstModel(i)] = min(modelAvgs(:,i,:));
-    % Temp: report
-    fprintf(1, '%s:\t',labelStruct(i).Label);
-%     fprintf(1, '\tMax R\x00B2 value = %0.3f,', max(x));
-%     fprintf(1, '\tMean R\x00B2 value = %0.3f,', mean(x));
-%     fprintf(1, '\tBest Model = %i', bestModel(i));
-%     fprintf(1, '\tWorst Model = %i\n', worstModel(i));
-    
+assert(length(labelStruct) == numROIs, 'Model fits and labels have unequal number of ROIs!');
+roimeans = zeros(numROIs, 1);
+roimaxes = zeros(numROIs, 1);
+for i = 1:numROIs
+    results = zeros(1,numPreds);
     % Variance partitioning:
     % R2 of full model MINUS R2 of model missing one predictor
-    % Significance level comes from bootstrapping 10,000 shuffled iterations
+    % Reports how much each predictor contributes to the overall regression
+    fprintf(1, '%s:\t',labelStruct(i).Label);
     fprintf(1, 'Variance attributable to:');
     for p = 1:numPreds+1
         if p == numPreds + 1
@@ -50,13 +30,22 @@ for i = 1:length(results)
             predName = predictorList{p};
             fullmodel = modelAvgs(:,i,end);
             limmodel = modelAvgs(:,i,p);
+            result = (fullmodel - limmodel) / fullmodel;
+            results(p) = result;
 %             fprintf('\t%s: %0.4f', predName, fullmodel - limmodel);
-            fprintf('\t%s: %0.4f', predName, (fullmodel - limmodel) / fullmodel);
+            fprintf('\t%s: %0.4f', predName, result);
         end
     end
     fprintf(1, '\n');
-    roimeans(i) = mean(x);
-    roimaxes(i) = max(x);
+    
+    % Write these results into a struct too
+    [r2, x] = max(results);
+    labelStruct(i).BestModel = predictorList{x};
+    labelStruct(i).R2 = r2; % also export R2 val
+    
+    % Measure consistency of each ROI
+    roimeans(i) = mean(modelFits(:,i,end));
+    roimaxes(i) = max(modelFits(:,i,end));
 end
 
 fprintf(1, '\n\nMean Max = %0.4f\tMax Mean = %0.4f\n', mean(roimaxes, 'omitnan'), max(roimeans));
@@ -66,29 +55,31 @@ fprintf(1, 'Max Max = %0.4f\tMean Mean = %0.4f\n', max(roimaxes), mean(roimeans,
 [v, i] = maxk(roimeans, 3);
 n = {labelStruct(i).Label};
 fprintf(1, '\nThe three most consistent ROIs are:\n');
-fprintf(1, '\t%s (mean R2 = %0.4f),\n', n{1}, v(1));
-fprintf(1, '\t%s (mean R2 = %0.4f), and \n', n{2}, v(2));
-fprintf(1, '\t%s (mean R2 = %0.4f).\n', n{3}, v(3));
+fprintf(1, '\t%s (mean R\x00B2 = %0.4f),\n', n{1}, v(1));
+fprintf(1, '\t%s (mean R\x00B2 = %0.4f), and \n', n{2}, v(2));
+fprintf(1, '\t%s (mean R\x00B2 = %0.4f).\n', n{3}, v(3));
 
 % Single-subject best model fit
 [v, i] = maxk(roimaxes, 3);
 n = {labelStruct(i).Label};
 fprintf(1, '\nThe three highest single-subject fits are in:\n');
-fprintf(1, '\t%s (max R2 = %0.4f),\n', n{1}, v(1));
-fprintf(1, '\t%s (max R2 = %0.4f), and \n', n{2}, v(2));
-fprintf(1, '\t%s (max R2 = %0.4f).\n', n{3}, v(3));
+fprintf(1, '\t%s (max R\x00B2 = %0.4f),\n', n{1}, v(1));
+fprintf(1, '\t%s (max R\x00B2 = %0.4f), and \n', n{2}, v(2));
+fprintf(1, '\t%s (max R\x00B2 = %0.4f).\n', n{3}, v(3));
 
 % Best ROI per model
 fprintf(1, '\nThe best ROI per predictor is:\n');
 for p = 1:numPreds+1
     if p == numPreds+1
         predName = 'Full Model';
-        [r2, roi] = max((modelAvgs(:,:,end)), [], 'omitnan');
+        r2 = 1;
+        [r2f, roi] = max((modelAvgs(:,:,end)), [], 'omitnan');
     else
         predName = predictorList{p};
         [r2, roi] = max((modelAvgs(:,:,end) - modelAvgs(:,:,p)) ./ modelAvgs(:,:,end), [], 'omitnan');
 %         [r2, roi] = max(modelAvgs(:,:,end) - modelAvgs(:,:,p), [], 'omitnan');
+        r2f = modelAvgs(:,roi,end);
     end
     roiName = labelStruct(roi).Label;
-    fprintf(1,'\t%s: %s (R2 = %0.4f)\n', predName, roiName, r2);
+    fprintf(1,'\t%s: %s (R\x00B2 = %0.4f of %0.4f)\n', predName, roiName, r2, r2f);
 end
